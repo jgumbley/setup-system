08a77df0285b121bad199ca8cab08d2f2cf24cab
system <system@MacBookPro.localdomain>
2025-04-20 16:49:31 +0100

    m

--- setup-mac/bootstrap.sh
+++ setup-mac/bootstrap.sh
@@ -2,5 +2,8 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 brew install git
 brew install fish
 brew install npm
+brew install python
+brew install ansible
+ansible-galaxy collection install community.general
 npm install -g @anthropic-ai/claude-code
 
