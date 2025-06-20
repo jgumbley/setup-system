f80a564a818d72d0ea3824a1b427265218115a89
JGumbley <j.gumbley@gmail.com>
2025-06-20 12:00:12 +0100

    handle apple vs intel

--- setup-mac/bootstrap.sh
+++ setup-mac/bootstrap.sh
@@ -1,7 +1,13 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-echo >> /Users/admin/.zprofile
-echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/admin/.zprofile
-eval "$(/opt/homebrew/bin/brew shellenv)"
+if [[ -f "/opt/homebrew/bin/brew" ]]; then
+    echo >> /Users/admin/.zprofile
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/admin/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+else
+    echo >> /Users/admin/.zprofile
+    echo 'eval "$(/usr/local/bin/brew shellenv)"' >> /Users/admin/.zprofile
+    eval "$(/usr/local/bin/brew shellenv)"
+fi
 brew install ansible
 ansible-galaxy collection install community.general
 
