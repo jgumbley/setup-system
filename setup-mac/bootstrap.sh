727543b1d7646d376c6483b126131e335010d08f
JGumbley <j.gumbley@gmail.com>
2025-06-20 11:10:20 +0100

    tweak bootstrap

--- setup-mac/bootstrap.sh
+++ setup-mac/bootstrap.sh
@@ -1,4 +1,4 @@
-NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 eval "$(/opt/homebrew/bin/brew shellenv)"
 brew install ansible
 ansible-galaxy collection install community.general
